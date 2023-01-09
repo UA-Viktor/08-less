1043bbc70c89ef86230872fe73ee36cac2ab7521
UA-Viktor <v.korenkov@icloud.com>
2023-01-09 21:37:56 +0200

    gogo88

---
+++ js/06-reduce.js
@@ -0,0 +1,53 @@
+const numbers = [5, 10, 15, 20, 25];
+
+// const total = numbers.reduce((acc, number) => {
+//     console.log('number', number);
+//     console.log('acc', acc);
+
+//     return acc + number;
+// }, 0);
+// console.log(total);
+
+
+
+
+const salary = {
+    mango: 100,
+    poly: 50, 
+    ajax: 150,
+}
+
+// console.log(Object.values(salary));
+
+// const totalSalary = Object.values(salary).reduce((total, value) => {
+//     return total += value;
+// })
+
+const totalSalary = Object.values(salary).reduce(
+    (total, value) => total += value,
+    0,
+);
+
+// console.log(totalSalary);
+
+
+
+
+
+
+
+
+const players = [
+    { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+    { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+    { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+    { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+    { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+];
+
+
+const totalTitimePlayed = players.reduce(
+    (totalTime, { timePlayed }) => totalTime + timePlayed,
+    0,
+);
+console.log(totalTitimePlayed);
