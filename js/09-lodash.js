615258ee339e7baff21d6bbd3227641eb27c9451
UA-Viktor <v.korenkov@icloud.com>
2023-01-11 17:59:50 +0200

    4-mod-end

---
+++ js/09-lodash.js
@@ -0,0 +1,42 @@
+// LODASH - Библиотека
+
+// lodash.com
+
+/* <script src="https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js"></script> */
+
+// isEmpty()
+
+// console.log(_.isEmpty({}));
+// console.log(_.isEmpty({a: 1}));
+
+// const user = {
+//     name: 'mango',
+//     location: {
+//         coords: [1, 2],
+//         city: 'Lviv',
+//     },
+// };
+
+// console.log(_.get(user, 'location.city'));
+
+// if (user && user.location && user.location.city) {
+//     console.log(user.location.city);
+// }
+
+// console.log(user?.location?.city);
+
+
+
+
+
+
+// UNION()
+
+
+console.log(_.union([1, 2, 3], [3, 4, 5]));
+
+
+// range()
+
+// sortBy()
+
